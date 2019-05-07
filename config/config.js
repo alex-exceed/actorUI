060f184701ca830627d0c76d7ad72059d4e9ca62
Alex Kucheryavenko <alex.kucheryavenko.exceed@gmail.com>
2019-05-07 11:15:16 +0300

    minor config change

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     baseOffersLink : 'https://www.amazon.com/gp/offer-listing/',
-    baseDomain     : 'http://ec2-3-91-99-130.compute-1.amazonaws.com:3000/',
+    baseDomain     : 'http://localhost:3000',
     baseFilesFolder: 'apify_storage/datasets/',
     baseUrl        : 'https://www.amazon.com/',
     user           : 'aiongame85@gmail.com',
